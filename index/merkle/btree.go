2af4c5c642e4d0871f73b5fcfe6b84efd18b4877
Oz Katz <ozkatz100@gmail.com>
2020-01-14 14:08:31 +0200

    WIP btree - from the looks of it, diffing becomes a much harder problem due to the non-determenistic structure of btrees - trees holding the same data might take different shapes, rendering the advantages of merkle trees a lot less relevant

--- index/merkle/btree.go
+++ index/merkle/btree.go
@@ -10,13 +10,13 @@ import (
 	"golang.org/x/xerrors"
 )
 
-type CachedNodeReader struct {
+type CachedNodeUpdater struct {
 	db          BTreeReader
 	cache       map[string]*model.Node
 	dirtyWrites map[string]*model.Node
 }
 
-func (c *CachedNodeReader) GetNode(addr string) (*model.Node, error) {
+func (c *CachedNodeUpdater) GetNode(addr string) (*model.Node, error) {
 	node, exists := c.cache[addr]
 	if !exists {
 		node, err := c.db.GetNode(addr)
@@ -34,14 +34,29 @@ func (c *CachedNodeReader) GetNode(addr string) (*model.Node, error) {
 	return nil, db.ErrNotFound
 }
 
-func (c *CachedNodeReader) WriteNode(addr string, node *model.Node) error {
+func (c *CachedNodeUpdater) WriteNode(addr string, node *model.Node) {
 	c.cache[addr] = node
 	c.dirtyWrites[addr] = node
+}
+
+func (c *CachedNodeUpdater) InvalidateNode(addr string) {
+	if _, exists := c.dirtyWrites[addr]; exists {
+		delete(c.dirtyWrites, addr)
+	}
+}
+
+func (c *CachedNodeUpdater) Flush(w BTreeWriter) error {
+	for addr, node := range c.dirtyWrites {
+		err := w.WriteNode(addr, node)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
-func NewCacheNodeReader(db BTreeReader) *CachedNodeReader {
-	return &CachedNodeReader{
+func NewCachedNodeUpdater(db BTreeReader) *CachedNodeUpdater {
+	return &CachedNodeUpdater{
 		db:          db,
 		cache:       make(map[string]*model.Node),
 		dirtyWrites: make(map[string]*model.Node),
@@ -174,7 +189,6 @@ func (t *BTree) PrefixScan(prefix, from string, amount int) (*ScanResults, error
 	return results, nil
 }
 
-// TODO: write path - return a new BTree on successful change
 func (t *BTree) Update(w BTreeWriter, branchingFactor int, changes []*TreeChange) (*BTree, error) {
 	root := t.root
 	nodes := make([]*model.Node, 0)
@@ -184,26 +198,13 @@ func (t *BTree) Update(w BTreeWriter, branchingFactor int, changes []*TreeChange
 	// - - make the change in the given node
 	// - - go to the parent - reflect the changes that need to happen - at a minimum - a pointer has changed
 	// - - this ^ happens recursively, until we get to the root node. This means we're creating a new root node for every modification
-	// once all changes are done, a lot of the internal nodes we created are no longer required - what is the rule?
-	// we can keep a reference count - every time we set a pointer to a node, pointerHash++
-	// on the other hand, every time we replace an existing pointer, pointerHash--
-	// decrementing to zero will "remove" this node from the list of nodes that need to be written eventually
-	// this is efficient since we only once scan the list of nodes so its o(n)
-
-	// once all changes have been reflected in the tree we write all nodes that still exist within the list
-	// do we really need to incr/decr? it's either one pointer or none since its ordered paths
-	// we can keep a set and simply add(x)/del(x) for previous pointers. we also need to do this for the root node!
-
 	// so to recap:
 	// - we make 1 change at a time, propagating changes up the tree and replacing the root node every time
 	// - once we've reflected all changes, and have the logical nodes built, we write the nodes to DB
 	// - at the end, return the last root generated and written
 
-	// I still think this is a garbage implementation and it's super complex but imma try to implement this shizz
-	nodecache := make(map[string]*model.Node)
-
 	// use a cached reader to avoid getting the same nodes all the time
-	r := NewCacheNodeReader(t.db)
+	r := NewCachedNodeUpdater(t.db)
 
 	// sort changes by path to ensure minimal retrievals and updates
 	sort.SliceStable(changes, func(i, j int) bool {
@@ -214,22 +215,121 @@ func (t *BTree) Update(w BTreeWriter, branchingFactor int, changes []*TreeChange
 	for _, change := range changes {
 		// find the node that should contain this change
 		currRoot := t.root
+		visited := make([]*model.Node, 0)
 		for {
 			node, err := r.GetNode(currRoot)
 			if err != nil {
 				return nil, err
 			}
+			visited = append(visited, node)
 			pointers := node.GetPointers()
-			for ind, entry := range node.GetEntries() {
+			entries := node.GetEntries()
+			for ind, entry := range entries {
 				cmp := strings.Compare(change.Path, entry.GetName())
+
 				if cmp == 0 {
 					// this is the node - we need to replace whatever value we had with the current change
 					// and propagate this up the tree resulting in a new root
+
+					// TODO: this flow is for an update - we need to also implement a deletion!!!
+					// TODO: btw, deletions only happen here, i.e. if the node exists
+					// TODO: if it does, we need to rebuild a node
+					newEntries := make([]*model.Entry, len(entries))
+
+					copy(newEntries, entries)
+					newEntries[ind] = change.Entry
+					newNode := &model.Node{
+						Entries:  newEntries,
+						Pointers: pointers,
+					}
+					newNodeAddr := ident.Hash(newNode)
+					oldNodeAddr := ident.Hash(node)
+					r.WriteNode(newNodeAddr, newNode)
+
+					// point parent node to this one instead
+					for i := len(visited) - 2; i >= 0; i-- { // -2 since we skip ourselves, we already updated
+						parent := nodes[i]
+						oldNodeAddr = ident.Hash(parent)
+						parentPointers := parent.GetPointers()
+						newParentPointers := make([]string, len(parentPointers))
+						copy(newParentPointers, parentPointers)
+						for pointerIndex, pointer := range parent.GetPointers() {
+							if strings.EqualFold(pointer, oldNodeAddr) {
+								garbageNode := newParentPointers[pointerIndex]
+								r.InvalidateNode(garbageNode) // no longer pointing there
+								newParentPointers[pointerIndex] = newNodeAddr
+							}
+						}
+						newNode := &model.Node{
+							Entries:  parent.GetEntries(),
+							Pointers: newParentPointers,
+						}
+						newNodeAddr = ident.Hash(newNode)
+						r.WriteNode(newNodeAddr, newNode)
+					}
+					root = newNodeAddr // traversed to the top
+
 				} else if cmp == -1 {
-					// changed path is bigger, we need to continue
+					// changed path is bigger, we need to continue to the next entry
+					continue
 				} else {
 					// change path is smaller, we need to either deep into the tree
 					// or if there's no pointer, update this node (splitting if needed, and propagating)
+					if len(pointers) >= ind+2 {
+						currRoot = pointers[ind+1] // traverse down to the next block
+					} else {
+						// we're at the correct leaf - we need to insert
+						if len(entries) >= branchingFactor {
+							// TODO: split
+							// what does a split looks like:
+							// 1. add entry and sort
+							// 2. pick median value, e.g. (1,5,7,9,11) => 7
+							// 3. create 2 nodes: one with (1,5) and one with (9,11)
+							// 4. write these nodes.
+							// 5. insert 7 into the parent node, sorted.
+							// 6. insert p(1,5) at 7's index, p(9,11) at 7's+1 index
+							// 7. of course, if no space in the parent, recurse the process upwards
+						} else {
+							// we have space here, let's add
+							newEntries := make([]*model.Entry, len(entries))
+
+							copy(newEntries, entries)
+							newEntries = append(newEntries, change.Entry)
+							sort.SliceStable(newEntries, func(i, j int) bool {
+								return strings.Compare(newEntries[i].GetName(), newEntries[j].GetName()) == -1
+							})
+							newNode := &model.Node{
+								Entries:  newEntries,
+								Pointers: pointers,
+							}
+							newNodeAddr := ident.Hash(newNode)
+							oldNodeAddr := ident.Hash(node)
+							r.WriteNode(newNodeAddr, newNode)
+
+							// point parent node to this one instead
+							for i := len(visited) - 2; i >= 0; i-- { // -2 since we skip ourselves, we already updated
+								parent := nodes[i]
+								oldNodeAddr = ident.Hash(parent)
+								parentPointers := parent.GetPointers()
+								newParentPointers := make([]string, len(parentPointers))
+								copy(newParentPointers, parentPointers)
+								for pointerIndex, pointer := range parent.GetPointers() {
+									if strings.EqualFold(pointer, oldNodeAddr) {
+										garbageNode := newParentPointers[pointerIndex]
+										r.InvalidateNode(garbageNode) // no longer pointing there
+										newParentPointers[pointerIndex] = newNodeAddr
+									}
+								}
+								newNode := &model.Node{
+									Entries:  parent.GetEntries(),
+									Pointers: newParentPointers,
+								}
+								newNodeAddr = ident.Hash(newNode)
+								r.WriteNode(newNodeAddr, newNode)
+							}
+							root = newNodeAddr // traversed to the top
+						}
+					}
 				}
 			}
 		}
@@ -240,13 +340,11 @@ func (t *BTree) Update(w BTreeWriter, branchingFactor int, changes []*TreeChange
 		// record the new root we received
 	}
 
-	// persist all new nodes
-	for _, node := range nodes {
-		err := w.WriteNode(ident.Hash(node), node)
-		if err != nil {
-			return nil, err
-		}
+	err := r.Flush(w) // write all new relevant nodes to storage.
+	if err != nil {
+		return nil, err
 	}
 
+	// return a new tree with the updated root
 	return &BTree{db: t.db, root: root}, nil
 }
